ec5c6712732a23398f37813014a272dc66d4224b
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2022-11-03 19:22:53 +0900

    Make mdx content render nicely

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,15 +1,23 @@
 import { format } from 'date-fns'
+import { serialize } from 'next-mdx-remote/serialize'
+import rehypeHighlight from 'rehype-highlight'
+import 'highlight.js/styles/night-owl.css'
 
-import { getPost } from '../../../utils/blog'
+import Content from './Content'
+import { getAllPosts, getPost } from '../../../utils/blog'
 
-export default function PostPage({
+export default async function PostPage({
   params,
-  searchParams,
 }: {
   params: { slug: string }
-  searchParams: { id: string }
 }) {
   const post = getPost(params.slug, ['title', 'date', 'content'])
+
+  const mdxSource = await serialize(post.content, {
+    mdxOptions: {
+      rehypePlugins: [rehypeHighlight],
+    },
+  })
 
   return (
     <main className="mx-auto max-w-prose px-4">
@@ -19,9 +27,19 @@ export default function PostPage({
           <time dateTime={post.date} className="text-sm text-slate-400">
             {format(new Date(post.date), 'yyyy MMM dd')}
           </time>
-          <div className="mt-4">{post.content}</div>
+          <div className="mt-4">
+            <Content mdxSource={mdxSource} />
+          </div>
         </article>
       </div>
     </main>
   )
+}
+
+export function generateStaticParams() {
+  const posts = getAllPosts(['slug'])
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }))
 }
