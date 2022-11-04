6cee3cc52c2009f681e2aa94af1d74567aa2dbe2
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2022-11-04 18:04:35 +0900

    Use ExternalLink for external links

--- app/blog/[slug]/Content.tsx
+++ app/blog/[slug]/Content.tsx
@@ -4,12 +4,15 @@ import 'highlight.js/styles/night-owl.css'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 
 import './mdx.css'
+import ExternalLink from '../../ExternalLink'
 
 type Props = {
   mdxSource: MDXRemoteSerializeResult
 }
 
-const components = {}
+const components = {
+  ExternalLink,
+}
 
 export default function Content({ mdxSource }: Props) {
   return <MDXRemote {...mdxSource} components={components} />
