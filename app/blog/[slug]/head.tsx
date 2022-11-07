fb890e6927f0d252b29733e5b9661429b55108e7
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2022-11-07 11:49:25 +0900

    Rename some variable names

--- app/blog/[slug]/head.tsx
+++ app/blog/[slug]/head.tsx
@@ -4,21 +4,21 @@ import { SITE_NAME, SITE_URL } from '../../../utils/constants'
 export default function PostHead({ params }: { params: { slug: string } }) {
   const post = getPost(params.slug, ['title', 'description'])
 
-  const pageUrl = `${SITE_URL}/blog/${params.slug}`
-  const pageTitle = `${post.title} | ${SITE_NAME}`
+  const url = `${SITE_URL}/blog/${params.slug}`
+  const title = `${post.title} | ${SITE_NAME}`
   const description = post.description
   const ogImage = ''
 
   return (
     <>
-      <title>{pageTitle}</title>
-      <meta property="og:title" content={pageTitle} />
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
       <meta name="description" content={description} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={ogImage} />
       <meta property="og:type" content="article" />
-      <meta property="og:url" content={pageUrl} />
-      <link rel="canonical" href={pageUrl} />
+      <meta property="og:url" content={url} />
+      <link rel="canonical" href={url} />
     </>
   )
 }
