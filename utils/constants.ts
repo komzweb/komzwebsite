881b9347230cb4d77109fda13c846147020bed23
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2022-11-08 11:53:17 +0900

    Add GitHub handle

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,4 @@
 export const SITE_URL = 'https://komzwebsite.vercel.app'
 export const SITE_NAME = 'komzWebsite'
-export const TWITTER_HANDLE = '@komzweb'
+export const GITHUB_HANDLE = 'komzweb'
+export const TWITTER_HANDLE = 'komzweb'
