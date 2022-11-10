5d9ddfab03221ab00e19f5efc652e8b0e72cf34d
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2022-11-10 18:45:18 +0900

    Add sticky header

--- app/blog/[slug]/layout.tsx
+++ app/blog/[slug]/layout.tsx
@@ -11,13 +11,17 @@ export default function PostLayout({
 }) {
   return (
     <Container>
-      <header className="flex h-32 items-center justify-between">
-        <Link href="/" className="text-3xl">
-          <FaHome />
-        </Link>
+      <header className="sticky top-0 z-50">
+        <div className="border-b border-slate-200 bg-white/95 dark:border-slate-800 dark:bg-slate-900/95">
+          <div className="flex h-16 items-center justify-between">
+            <Link href="/" className="text-3xl">
+              <FaHome />
+            </Link>
 
-        <div className="text-2xl">
-          <Social />
+            <div className="text-2xl">
+              <Social />
+            </div>
+          </div>
         </div>
       </header>
 
