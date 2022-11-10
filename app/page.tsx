06024870c94fa0fd0d475df3b96a40ccd33bb6b8
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2022-11-10 18:26:53 +0900

    Add link to post page

--- app/page.tsx
+++ app/page.tsx
@@ -36,13 +36,21 @@ export default function RootPage() {
               className="border-t border-slate-200 py-4 dark:border-slate-800"
             >
               <h2 className="text-2xl font-bold dark:text-slate-100">
-                <Link href={`/blog/${post.slug}`} className="hover:underline">
-                  {post.title}
-                </Link>
+                <Link href={`/blog/${post.slug}`}>{post.title}</Link>
               </h2>
-              <time dateTime={post.date} className="text-sm text-slate-400">
-                {format(new Date(post.date), 'yyyy MMM dd')}
-              </time>
+              <div className="mt-1">
+                <time dateTime={post.date} className="text-sm text-slate-400">
+                  {format(new Date(post.date), 'yyyy MMM dd')}
+                </time>
+              </div>
+              <div className="mt-4">
+                <Link
+                  href={`/blog/${post.slug}`}
+                  className="text-blue-500 hover:text-blue-400"
+                >
+                  Read Post
+                </Link>
+              </div>
             </article>
           ))}
         </article>
