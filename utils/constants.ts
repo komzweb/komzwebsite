9e0da84e464204a8db912d407665a99b2d6a5ff4
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2022-12-27 11:57:02 +0900

    Update site name

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_URL = 'https://komzwebsite.vercel.app'
-export const SITE_NAME = 'komzWebsite'
+export const SITE_NAME = 'KOMZ WEBSITE'
 export const GITHUB_HANDLE = 'komzweb'
 export const TWITTER_HANDLE = 'komzweb'
