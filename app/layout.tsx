d2d1f932f3189cb6b563942d76ac137a9ac5a32e
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2023-05-03 17:45:47 +0900

    Use `next/font` instead of `@next/font`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 
 import './globals.css'
 
 const inter = Inter({
   subsets: ['latin'],
-  variable: '--font-inter',
+  display: 'swap',
 })
 
 export default function RootLayout({
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="en" className={inter.className}>
       <head />
       <body className="text-slate-900 dark:bg-slate-900 dark:text-slate-300">
         {children}
