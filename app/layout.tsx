6c35fbcfbc9c6dc96d4dcc43c4c83015f4fd4583
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2023-05-04 14:40:16 +0900

    Move metadata to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,43 @@
+import './globals.css'
 import { Inter } from 'next/font/google'
 
-import './globals.css'
+import { SITE_NAME, SITE_URL, TWITTER_HANDLE } from '../utils/constants'
 
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
+const inter = Inter({ subsets: ['latin'] })
+
+const description = 'Built with Next.js, Tailwind CSS, and Vercel'
+
+export const metadata = {
+  title: SITE_NAME,
+  description: description,
+  openGraph: {
+    title: SITE_NAME,
+    description: description,
+    type: 'website',
+    url: SITE_URL,
+  },
+  alternates: {
+    canonical: SITE_URL,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: `@${TWITTER_HANDLE}`,
+  },
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [
+      {
+        url: '/favicon/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  },
+  manifest: '/favicon/site.webmanifest',
+}
 
 export default function RootLayout({
   children,
@@ -14,7 +46,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <head />
       <body className="text-slate-900 dark:bg-slate-900 dark:text-slate-300">
         {children}
       </body>
