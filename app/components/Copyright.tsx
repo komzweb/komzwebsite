da0f157447eac2798ffc724ee6c53ee01d92c49b
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2023-05-05 14:34:45 +0900

    Add style

--- app/components/Copyright.tsx
+++ app/components/Copyright.tsx
@@ -2,7 +2,7 @@ import { SITE_NAME } from '../../utils/constants'
 
 export default function Copyright() {
   return (
-    <span>
+    <span className="text-sm text-slate-400">
       © {new Date().getFullYear()} {SITE_NAME}
     </span>
   )
