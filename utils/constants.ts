1ea1d92f5b85dd6e5c58404e0c1e938646ebd4b4
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2023-05-05 13:50:00 +0900

    Add several constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,10 @@
 export const SITE_URL = 'https://komzwebsite.vercel.app'
 export const SITE_NAME = 'KOMZ WEBSITE'
-export const GITHUB_HANDLE = 'komzweb'
+
+export const PROFILE_NAME = 'komz'
+export const PROFILE_BIO = `Hi, I am ${PROFILE_NAME}. I am a web engineer. I love: building web apps with Next.js, Tailwind CSS, and Vercel | integrating AI and Blockchain into apps | promoting technology education. I also love learning new things.`
+
 export const TWITTER_HANDLE = 'komzweb'
+export const GITHUB_HANDLE = 'komzweb'
+export const MEDIUM_HANDLE = 'komzweb'
+export const NOTE_HANDLE = 'komzweb'
