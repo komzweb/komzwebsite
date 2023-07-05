5eceb177992cae6b2d88f578585cc83472f184e5
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2023-07-05 11:08:08 +0900

    Update profile bio

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,7 @@ export const SITE_URL = 'https://komzwebsite.vercel.app'
 export const SITE_NAME = 'KOMZ WEBSITE'
 
 export const PROFILE_NAME = 'komz'
-export const PROFILE_BIO = `Hi, I am ${PROFILE_NAME}. I am a web developer. I love building web apps with Next.js, Tailwind CSS, and Vercel. Currently, I am working on the development of AI applications using vector databases. I also love learning new things.`
+export const PROFILE_BIO = `Hi, I am ${PROFILE_NAME}. I am a web developer. My primary language is TypeScript. I specialize in building web apps using Next.js, Tailwind CSS, and Vercel. I love learning new things!`
 
 export const TWITTER_HANDLE = 'komzweb'
 export const GITHUB_HANDLE = 'komzweb'
