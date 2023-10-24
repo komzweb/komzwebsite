0b9ca0b1c7de833faa4df1119dda967c2b86f752
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2023-10-25 06:54:27 +0900

    Update profile bio

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,7 @@ export const SITE_URL = 'https://komzwebsite.vercel.app'
 export const SITE_NAME = 'KOMZ WEBSITE'
 
 export const PROFILE_NAME = 'komz'
-export const PROFILE_BIO = `Hi, I am ${PROFILE_NAME}. I am a web developer. My primary language is TypeScript. I specialize in building web apps using Next.js, Tailwind CSS, and Vercel. I love learning new things!`
+export const PROFILE_BIO = `こんにちは、コムズです。プログラマーです。Webアプリケーション開発、AIの研究をしています。OpenAI、Anthropic、Adobe、Vercelなどのプロダクトに関する情報を、X/Twitterやnoteに投稿しています。`
 
 export const TWITTER_HANDLE = 'komzweb'
 export const GITHUB_HANDLE = 'komzweb'
