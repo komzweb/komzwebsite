534e65e6644c6967705b3452c9c9387f3f6f6178
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2023-11-15 18:33:57 +0900

    Update Geist imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
-import { GeistSans, GeistMono } from 'geist/font'
+import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from 'geist/font/sans'
 import { ThemeProvider } from '@/components/theme-provider'
 import { SITE_NAME, SITE_URL, TWITTER_HANDLE } from '@/utils/constants'
 
