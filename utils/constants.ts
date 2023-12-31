5f7ee3dce80f567b98c9efc4f482015e45b5480e
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2023-12-31 12:16:57 +0900

    Update profile bio

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,7 @@ export const SITE_URL = 'https://komzwebsite.vercel.app'
 export const SITE_NAME = 'KOMZ WEBSITE'
 
 export const PROFILE_NAME = 'komz'
-export const PROFILE_BIO = `Hi, I am komz. I am a web developer. I develop web applications using Next.js and research on how to use AI. I also post product information from OpenAI, Adobe, Vercel and others on X/Twitter and note.`
+export const PROFILE_BIO = `Hi, I am ${PROFILE_NAME}. I am a web developer. I love building web applications using Next.js. I post about practical uses of AI products and tools for developers, new discoveries from papers, etc. on social media.`
 
 export const TWITTER_HANDLE = 'komzweb'
 export const GITHUB_HANDLE = 'komzweb'
