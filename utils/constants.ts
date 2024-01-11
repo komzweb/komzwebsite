dc1b3761f8fcaef657345c3eace5fefd23303023
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2024-01-11 10:42:34 +0900

    Update website URL to new domain

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,5 @@
-export const SITE_URL = 'https://komzwebsite.vercel.app'
-export const SITE_NAME = 'KOMZ WEBSITE'
+export const SITE_URL = 'https://komzweb.net'
+export const SITE_NAME = 'KOMZ WEB'
 
 export const PROFILE_NAME = 'komz'
 export const PROFILE_BIO = `Hi, I am ${PROFILE_NAME}. I am a web developer. I love building web applications using Next.js. I post about practical uses of AI products and tools for developers, new discoveries from papers, etc. on social media.`
