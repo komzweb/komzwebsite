f5100453864ce68d1621d5ee7d4a7b5cec6ada82
Koshi Matsumoto <116416464+komzweb@users.noreply.github.com>
2024-01-20 16:14:49 +0900

    Update profile bio

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,7 @@ export const SITE_URL = 'https://komzweb.net'
 export const SITE_NAME = 'KOMZ WEB'
 
 export const PROFILE_NAME = 'komz'
-export const PROFILE_BIO = `Hi, I am ${PROFILE_NAME}. I am a web developer. I love building web applications using Next.js. I post about practical uses of AI products and tools for developers, new discoveries from papers, etc. on social media.`
+export const PROFILE_BIO = `Hi, I am ${PROFILE_NAME}. I am a web developer. I specialize in TypeScript and Next.js. I like creative activities using AI and exploring its future. I post useful information and interesting discoveries on social media.`
 
 export const TWITTER_HANDLE = 'komzweb'
 export const GITHUB_HANDLE = 'komzweb'
